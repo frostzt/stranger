3c4eb79c642e160520f406e78d189330de25f7a4
frostzt <aidenfrostbite@gmail.com>
2021-11-06 10:59:52 +0530

    setting up the users endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,14 @@
+import express from 'express';
+
 import ExpressApplication from './bootstrap';
 
 const PORT = process.env.PORT || 5000;
 
-const app = new ExpressApplication(PORT);
+const app = new ExpressApplication(
+  PORT,
+  [express.json({ limit: '10kb' }), express.urlencoded({ extended: true, limit: '10kb' })],
+  [],
+);
 
+// Start the server
 app.start();
--- server/src/bootstrap.ts
+++ server/src/bootstrap.ts
@@ -1,24 +1,56 @@
 import path from 'path';
+import morgan from 'morgan';
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
 
 // Load the env vars based on the current NODE_ENV
 dotenv.config({ path: `../.env.${process.env.NODE_ENV}` });
 
+interface Route {
+  route: string;
+  handler: any;
+}
+
 class ExpressApplication {
   public app: Application;
 
-  constructor(private port: string | number) {
+  constructor(private port: string | number, private middlewares: any[], private routes: Route[]) {
     this.app = express();
     this.port = port;
+
+    // Initialize
+    this.setupMiddlewares(middlewares);
+    this.setupRoutes(routes);
+    this.configureAssets();
+    this.setupLogger();
+  }
+
+  // Configure and plug in middlewares
+  private setupMiddlewares(middlewaresArr: any[]) {
+    middlewaresArr.forEach((middleware) => {
+      this.app.use(middleware);
+    });
+  }
+
+  // Configure routes
+  private setupRoutes(routesArr: Route[]) {
+    routesArr.forEach(({ route, handler }) => {
+      this.app.use(route, handler);
+    });
   }
 
   private configureAssets() {
     this.app.use(express.static(path.join(__dirname, '../public')));
   }
 
+  // The logger will work only for the development env
+  private setupLogger() {
+    if (process.env.NODE_ENV === 'development') {
+      this.app.use(morgan('dev'));
+    }
+  }
+
   public start() {
-    this.configureAssets();
     this.app.listen(this.port, () => {
       console.log(`Application started listening on port ${this.port}`);
     });
