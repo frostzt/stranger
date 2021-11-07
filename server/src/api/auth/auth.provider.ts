36ff82f2cd7d2df2a7116ab237a9fec0f201e175
frostzt <aidenfrostbite@gmail.com>
2021-11-07 18:36:25 +0530

    implementing DB schema

--- server/src/api/auth/auth.provider.ts
+++ server/src/api/auth/auth.provider.ts
@@ -1,22 +1,8 @@
 import { sign } from 'jsonwebtoken';
 
-class AuthenticationProvider {
-  private static authProviderInstance: AuthenticationProvider;
+const signToken = (payload: any) =>
+  sign(payload, process.env.JWT_SECRET!, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-  public static getInstance(): AuthenticationProvider {
-    if (!AuthenticationProvider.authProviderInstance) {
-      AuthenticationProvider.authProviderInstance = new AuthenticationProvider();
-    }
-
-    return AuthenticationProvider.authProviderInstance;
-  }
-
-  // The envs are always available NOT to be provided in code
-  public signToken(payload: any) {
-    return sign(payload, process.env.JWT_SECRET!, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-  }
-}
-
-export default AuthenticationProvider;
+export default signToken;
