eec2db35e76ad26f65c5998f47bd9c90b32f396f
frostzt <aidenfrostbite@gmail.com>
2021-11-09 17:59:13 +0530

    working on user schema

--- server/src/models/User.model.ts
+++ server/src/models/User.model.ts
@@ -1,6 +1,32 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
 const User = mongoose.model('user', userSchema);
 
